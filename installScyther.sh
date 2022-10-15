b20a1ef168ef7e7125ed5bdd0a594ac87d6cb671
Kirill Romanenko <romanenko.kirill1995@gmail.com>
2022-10-15 16:17:56 +0300

    update

--- installScyther.sh
+++ installScyther.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt-get install python-is-python3
-sudo apt install python-pip
+sudo apt install python3-pip
 pip install -U \
   -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04 \
   wxPython
 sudo apt-get install git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0
 sudo apt-get install graphviz
+sudo apt-get install cmake build-essential flex bison gcc-multilib
